4a1e504812f1586ab76ee31b11a9d714e05bd321
KafaloAlex <ckalexis20@gmail.com>
2022-03-19 12:09:36 +0000

    Coverage

--- test_model.py
+++ test_model.py
@@ -3,35 +3,33 @@ import pytest
 
 # - Agent : 
 class Agent:
-    AGENT = script.Agent(30, agreeableness=1)
+    def setup_method(self):
+        self.agent = script.Agent(30, agreeableness=1)
     #   - modifier un attribut position
     def test_modify_position(self):
-        agent = self.AGENT
-        agent.position = 5
-        assert agent.position == 5
+        self.agent.position = 5
+        assert self.agent.position == 5
         
     #   - récupérer un attribut position
     def test_get_position(self):
-        agent = self.AGENT
-        assert agent.position == 30
+        assert self.agent.position == 30
     
     #   - assigner un dictionnaire en tant qu'attributs
     def test_set_agent_attributes(self):
-        agent = self.AGENT
-        assert agent.agreeableness == 1
+        assert self.agent.agreeableness == 1
 
 # - Position :
 class Position:
-    POSITION = script.Position(100, 34)
+    def setup_method(self):
+        self.position = script.Position(100, 34)
+        
     #   - modifier un attribut longitude_degrees
     def test_longitude_degrees(self):
-        position =self.POSITION
-        assert position.longitude_degrees == 100
+        assert self.position .longitude_degrees == 100
         
     #   - modifier un attribut latitude_degrees
     def test_latitude_degrees(self):
-        position = self.POSITION
-        assert position.latitude_degrees == 34
+        assert self.position.latitude_degrees == 34
     
     #   - modifier un attribut longitude_degrees avec une valeur supérieure à 180 renvoie une erreur. 
     def test_longitude_degrees_range(self):
@@ -45,59 +43,129 @@ class Position:
 
     #   - récupérer une latitude
     def test_latitude(self):
-        position = self.POSITION
+        # position = self.POSITION
         # print(position.latitude)
-        assert position.latitude == 0.5934119456780721
+        assert self.position.latitude == 0.5934119456780721
         
     #   - récupérer une longitude
     def test_longitude(self):
-        position = self.POSITION
         # print(position.longitude)
-        assert position.longitude == 1.7453292519943295
+        assert self.position.longitude == 1.7453292519943295
 
 # - Zone :
 class TestZone:
-    POSITION1 = script.Position(100, 33)
-    POSITION2 = script.Position(101, 34)
-    ZONE = script.Zone(POSITION1, POSITION2)
-    AGENT = script.Agent(POSITION1, agreeableness=1)
+    def setup_method(self):
+        self.position1 = script.Position(100, 33)
+        self.position2 = script.Position(101, 34)
+        self.zone = script.Zone(self.position1, self.position2)
+        script.Zone._initialize_zones()
+        agent = script.Agent(self.position1, agreeableness=1)
+        self.zone.inhabitants = [agent]
+        
+    def teardown_method(self):
+        script.Zone.ZONES = []
     
+    def test_get_zones(self):
+        assert len(script.Zone.ZONES) == 64800
+
+    # - Zone :
+    def test_zone_that_contains(self):
+        position = self.position1
+        found_zone = script.Zone.find_zone_that_contains(position)
+        assert position.longitude >= min(found_zone.corner1.longitude, found_zone.corner2.longitude)
+        assert position.longitude < max(found_zone.corner1.longitude, found_zone.corner2.longitude)
+        assert position.latitude >= min(found_zone.corner1.latitude, found_zone.corner2.latitude)
+        assert position.latitude < max(found_zone.corner1.latitude, found_zone.corner2.latitude)
+
+
     #   - trouver une zone qui contient une position
     def test_find_zone_that_contains(self):
-        found_zone = script.Zone.find_zone_that_contains(self.POSITION1)
-        assert found_zone.corner1.longitude == self.ZONE.corner1.longitude
+        found_zone = script.Zone.find_zone_that_contains(self.position1)
+        assert found_zone.contains(self.position1)
 
     #   - ajouter un habitant dans une zone
     def test_add_inhabitant_in_zone(self):
-        self.ZONE.add_inhabitant(self.AGENT)
-        assert len(self.ZONE.inhabitants) == 1
-
-    #   - récupérer toutes les instances Zone (Zone.ZONES)
-    # On devrait avoir exactement 64800 zones
-    def test_get_zones(self):
-        assert len(script.Zone.ZONES) == 64800
-        # print(len(script.Zone.ZONES))
+        agent = script.Agent(self.position1, agreeableness=1)
+        self.zone.add_inhabitant(agent)
+        assert len(self.zone.inhabitants) == 2
 
     #   - récupérer la densité de population d'une zone
     def test_get_population_density(self):
-        assert self.ZONE.population_density() == 8.087793508722422e-05
+        assert self.zone.population_density() == 8.087793508722422e-05
 
     #   - récupérer l'agréabilité moyenne d'une zone
     def test_get_average_agreeableness(self):
-        assert self.ZONE.average_agreeableness() == 1
+        assert self.zone.average_agreeableness() == 1
+
 
 # - AgreeablenessGraph :
-#   - récupérer un titre
-#   - récupérer x_label
-#   - récupérer y_label
-#   - récupérer xy_values sous forme de tuples
-#   - la première valeur de xy_values est la densité de population moyenne
-#   - la seconde valeur de xy_values est l'agréabilité moyenne
 
-# - IncomeGraph :
-#   - récupérer un titre
-#   - récupérer x_label
-#   - récupérer y_label
-#   - récupérer xy_values sous forme de tuples
-#   - la première valeur de xy_values est l'âge
-#   - la seconde valeur de xy_values est le revenu
+class TestAgreeablenessGraph:
+    
+    def setup_method(self):
+        script.Zone._initialize_zones()
+        self.zone = script.Zone.ZONES[0]
+        self.graph = script.AgreeablenessGraph()
+        self.zones = script.Zone.ZONES
+        for i in range(0, 10):
+            self.zone.add_inhabitant(script.Agent(script.Position(-180, -89), agreeableness=1))
+    
+    # Remet tout à zéro après exécution des Tests
+    def teardown_method(self):
+        script.Zone.ZONES = []
+        
+    GRAPH = script.AgreeablenessGraph()
+    #     - récupérer un titre
+    def test_title(self):
+        assert self.GRAPH.title == "Nice people live in the countryside"
+        
+    #   - récupérer x_label
+    def test_x_label(self):
+        assert self.GRAPH.x_label == "population density"
+    #   - récupérer y_label
+    def test_y_label(self):
+        assert self.GRAPH.y_label == "agreeableness"
+        
+    #   - récupérer xy_values sous forme de tuples
+    def test_xy_values(self):
+        assert len(self.graph.xy_values(self.zones)) == 2
+
+    #   - la première valeur de xy_values est la densité de population moyenne
+    def test_average_population_density(self):
+        assert self.graph.xy_values(self.zones)[0][0] == self.zone.population_density()
+        
+    #   - la seconde valeur de xy_values est l'agréabilité moyenne
+    def test_average_agreeableness(self):
+        assert self.graph.xy_values(self.zones)[1][0] == self.zone.average_agreeableness()
+
+    # - IncomeGraph :
+class TestIncomeGraph:
+    
+    def setup_method(self):
+        script.Zone._initialize_zones()
+        self.zone = script.Zone.ZONES[0]
+        self.graph = script.IncomeGraph()
+        self.zones = script.Zone.ZONES
+        for _ in range(0, 10):
+            self.zone.add_inhabitant(script.Agent(script.Position(-180, -89), income=40, age=20))
+
+    def setup_teardown(self):
+        script.Zone.ZONES = []
+
+    def test_title(self):
+        assert self.graph.title == 'Older people have more money'
+
+    def test_x_label(self):
+        assert self.graph.x_label == 'age'
+
+    def test_y_label(self):
+        assert self.graph.y_label == 'income'
+
+    def test_xy_values(self):
+        assert len(self.graph.xy_values(self.zones)) == 2
+
+    def test_age(self):
+        assert self.graph.xy_values(self.zones)[0][20] == 20
+
+    def test_average_income_by_age(self):
+        assert self.graph.xy_values(self.zones)[1][20] == 40
